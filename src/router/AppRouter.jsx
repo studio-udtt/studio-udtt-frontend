f155bb9268d8eb03841d8771c14aae716f7bf746
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-20 21:06:23 +0900

    fix: add admin router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import MainLayout from "../layouts/MainLayout";
+import AdminLayout from "../layouts/AdminLayout";
 
 import HomePage from "../pages/HomePage";
 import RecruitPage from "../pages/RecruitPage";
 import AboutPage from "../pages/AboutPage";
 import ProjectDetailPage from "../pages/ProjectDetailPage";
 import ProjectApplicationPage from "../pages/ProjectApplicationPage";
+
+import AdminLoginPage from "../pages/admin/AdminLoginPage";
+import AdminDashboardPage from "../pages/admin/AdminDashboardPage";
 
 export default function AppRouter() {
   return (
@@ -20,6 +25,12 @@ export default function AppRouter() {
             path="/projects/:projectId/apply"
             element={<ProjectApplicationPage />}
           />
+        </Route>
+
+        <Route path="/admin/login" element={<AdminLoginPage />} />
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboardPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
