668d09d60b68b58b4d71cb1b486703818ef3d421
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-20 20:49:37 +0900

    fix: add css file

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from "./App";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import "./styles/layout.css";
+import "./styles/home.css";
+import "./styles/recruit.css";
+import "./styles/about.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
